f481b5f73deebc5d9ea88674b837bbc9656fe0f2
Amin Moghaddam <amin@pyth.network>
2024-01-28 11:21:46 +0100

    Use price feed id as the name of WETH contract for consistency

--- per_multicall/src/WETH9.sol
+++ per_multicall/src/WETH9.sol
@@ -20,7 +20,8 @@
 pragma solidity ^0.8.0;
 
 contract WETH9 {
-    string public name = "Wrapped Ether";
+    string public name =
+        "c96458d393fe9deb7a7d63a0ac41e2898a67a7750dbd166673279e06c868df0a"; // ETH/USD pricefeed id
     string public symbol = "WETH";
     uint8 public decimals = 18;
 
