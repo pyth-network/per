f26e794b33765c2e98093ca37ff6c26051be3b64
Amin Moghaddam <amin@pyth.network>
2024-03-26 16:34:45 +0100

    minor cleanup

--- auction-server/migrations/20240326063340_bids.up.sql
+++ auction-server/migrations/20240326063340_bids.up.sql
@@ -7,17 +7,7 @@ CREATE TABLE bid
     target_contract BYTEA          NOT NULL,
     target_calldata BYTEA          NOT NULL,
     bid_amount      NUMERIC(80, 0) NOT NULL,
-    status          TEXT           NOT NULL, -- pending, lost, won
-    auction_id      UUID, -- should be linked to the auction table in the future
-    removal_time    TIMESTAMP -- when the au
-);
-
-CREATE TABLE auction
-(
-    id              UUID PRIMARY KEY,
-    creation_time   TIMESTAMP      NOT NULL,
-    permission_key  BYTEA          NOT NULL,
-    chain_id        TEXT           NOT NULL,
-    status          TEXT           NOT NULL, -- success,failure
-    tx_hash         BYTEA           NOT NULL
+    status          TEXT           NOT NULL, -- pending, lost, submitted
+    auction_id      UUID, -- TODO: should be linked to the auction table in the future
+    removal_time    TIMESTAMP -- TODO: should be removed and read from the auction table in the future
 );
