dc4a1a85ea49c0091c795ae60a687189d372786f
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-04-17 20:13:44 +0400

    Fix review comments

--- per_multicall/src/SigVerify.sol
+++ per_multicall/src/SigVerify.sol
@@ -22,7 +22,7 @@ contract SigVerify is EIP712Upgradeable {
      * @param hashedData The eip712 hashed data constructed from the execution parameters with the provided type.
      * @param signer The expected signer address.
      * @param signature The signature to be verified.
-     * @param deadline The deadline timestamp until which the signature is valid.
+     * @param deadline The deadline timestamp in seconds until which the signature is valid.
      * @dev Throws `InvalidSignature` if the signature is invalid or doesn't match the signer.
      * Throws `ExpiredSignature` if the signature has expired based on the provided deadline.
      * Throws `SignatureAlreadyUsed` if the signature has already been used.
@@ -34,6 +34,14 @@ contract SigVerify is EIP712Upgradeable {
         bytes memory signature,
         uint256 deadline
     ) public view {
+        if (block.timestamp > deadline) {
+            revert ExpiredSignature();
+        }
+
+        if (_signatureUsed[signature]) {
+            revert SignatureAlreadyUsed();
+        }
+
         bytes32 digest = _hashTypedDataV4(
             keccak256(
                 abi.encode(
@@ -52,14 +60,6 @@ contract SigVerify is EIP712Upgradeable {
 
         if (_signer == address(0) || _signer != signer) {
             revert InvalidSignature();
-        }
-
-        if (block.timestamp > deadline) {
-            revert ExpiredSignature();
-        }
-
-        if (_signatureUsed[signature]) {
-            revert SignatureAlreadyUsed();
         }
     }
 
