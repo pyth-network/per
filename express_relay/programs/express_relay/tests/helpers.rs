53d4cd0193e56c918b9bc1f9f1a4b7099724dbcc
--systemdf <anirudhtx@gmail.com>
2024-05-01 23:28:02 -0400

    add 1 moar test

--- express_relay/programs/express_relay/tests/helpers.rs
+++ express_relay/programs/express_relay/tests/helpers.rs
@@ -2,7 +2,7 @@ use anchor_lang::{system_program, prelude::*};
 use solana_program_test::ProgramTestContext;
 use solana_sdk::{account::Account, instruction::Instruction, signature::Keypair, transaction::Transaction, signer::Signer};
 use anchor_lang::{ToAccountMetas, InstructionData};
-use express_relay::{state::SEED_METADATA, InitializeArgs, SetRelayerArgs, accounts::{Initialize, SetRelayer}};
+use express_relay::{state::SEED_METADATA, InitializeArgs, SetRelayerArgs, SetSplitsArgs, accounts::{Initialize, SetRelayer, SetSplits}};
 
 pub async fn initialize(program_context: &mut ProgramTestContext, payer: Keypair, admin: Pubkey, relayer_signer: Pubkey, relayer_fee_receiver: Pubkey, split_protocol_default: u64, split_relayer: u64) -> Account {
     let express_relay_metadata = Pubkey::find_program_address(&[SEED_METADATA], &express_relay::id()).0;
@@ -73,6 +73,45 @@ pub async fn set_relayer(program_context: &mut ProgramTestContext, admin: Keypai
     program_context
         .banks_client
         .process_transaction(set_relayer_tx)
+        .await
+        .unwrap();
+
+    let express_relay_metadata_acc = program_context
+        .banks_client
+        .get_account(express_relay_metadata)
+        .await
+        .unwrap()
+        .unwrap();
+
+    return express_relay_metadata_acc;
+}
+
+pub async fn set_splits(program_context: &mut ProgramTestContext, admin: Keypair, split_protocol_default: u64, split_relayer: u64) -> Account {
+    let express_relay_metadata = Pubkey::find_program_address(&[SEED_METADATA], &express_relay::id()).0;
+
+    let set_splits_ix = Instruction {
+        program_id: express_relay::id(),
+        data:
+        express_relay::instruction::SetSplits {
+            data: SetSplitsArgs {
+                split_protocol_default: split_protocol_default,
+                split_relayer: split_relayer,
+            }
+        }.data(),
+        accounts: SetSplits {
+            admin: admin.pubkey(),
+            express_relay_metadata: express_relay_metadata,
+        }
+        .to_account_metas(None),
+    };
+
+    let mut set_splits_tx = Transaction::new_with_payer(&[set_splits_ix],Some(&admin.pubkey()));
+    let recent_blockhash = program_context.last_blockhash.clone();
+
+    set_splits_tx.partial_sign(&[&admin], recent_blockhash);
+    program_context
+        .banks_client
+        .process_transaction(set_splits_tx)
         .await
         .unwrap();
 
--- express_relay/programs/express_relay/tests/test_set_splits.rs
+++ express_relay/programs/express_relay/tests/test_set_splits.rs
@@ -4,10 +4,10 @@ use anchor_lang::prelude::*;
 use solana_program_test::{ProgramTest, tokio};
 use solana_sdk::{account::Account, signature::Keypair, signer::Signer};
 use express_relay::{state::ExpressRelayMetadata};
-use helpers::{initialize, set_relayer};
+use helpers::{initialize, set_splits};
 
 #[tokio::test]
-async fn test_set_relayer() {
+async fn test_set_splits() {
     let mut program_test = ProgramTest::new(
         "express_relay",
         express_relay::id(),
@@ -40,12 +40,12 @@ async fn test_set_relayer() {
 
     initialize(&mut program_context, payer, admin.pubkey(), relayer_signer, relayer_fee_receiver, split_protocol_default, split_relayer).await;
 
-    let new_relayer_signer = Keypair::new().pubkey();
-    let new_relayer_fee_receiver = Keypair::new().pubkey();
+    let new_split_protocol_default: u64 = 1000;
+    let new_split_relayer: u64 = 3000;
 
-    let express_relay_metadata_acc_2 = set_relayer(&mut program_context, admin, new_relayer_signer, new_relayer_fee_receiver).await;
+    let express_relay_metadata_acc_2 = set_splits(&mut program_context, admin, new_split_protocol_default, new_split_relayer).await;
 
     let express_relay_metadata_data_2 = ExpressRelayMetadata::try_deserialize(&mut express_relay_metadata_acc_2.data.as_ref()).unwrap();
-    assert_eq!(express_relay_metadata_data_2.relayer_signer, new_relayer_signer);
-    assert_eq!(express_relay_metadata_data_2.relayer_fee_receiver, new_relayer_fee_receiver);
+    assert_eq!(express_relay_metadata_data_2.split_protocol_default, new_split_protocol_default);
+    assert_eq!(express_relay_metadata_data_2.split_relayer, new_split_relayer);
 }
