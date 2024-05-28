35670b63f98576ae1027c309395c1f556bcc075a
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-05-28 16:20:58 +0200

    Add index for bid model

---
+++ auction-server/migrations/20240528141627_bid_index.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX bid_profile_id_idx;
+DROP INDEX bid_initiation_time_idx;
---
+++ auction-server/migrations/20240528141627_bid_index.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX bid_profile_id_idx ON bid (profile_id);
+CREATE INDEX bid_initiation_time_idx ON bid (initiation_time);
