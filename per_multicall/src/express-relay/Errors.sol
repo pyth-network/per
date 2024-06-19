56683ba6ab4bb9d5c555326b0c04c7a8834fc932
Amin Moghaddam <amin@pyth.network>
2024-06-19 07:53:48 +0200

    Remove unused comment

--- per_multicall/src/express-relay/Errors.sol
+++ per_multicall/src/express-relay/Errors.sol
@@ -6,10 +6,6 @@ import "../CommonErrors.sol";
 // Signature: 0x868a64de
 error InvalidPermission();
 
-//// The new contract does not have the same magic value as the old one.
-//// Signature: 0x4ed848c1
-//error InvalidMagicValue();
-
 // Signature: 0x0601f697
 error InvalidFeeSplit();
 
