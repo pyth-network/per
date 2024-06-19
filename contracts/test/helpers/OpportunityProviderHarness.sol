ba458d841c42d82897f763a870b115ded0a972ae
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-06-19 13:53:21 +0200

    Merge with main

--- contracts/test/helpers/OpportunityProviderHarness.sol
+++ contracts/test/helpers/OpportunityProviderHarness.sol
@@ -1,14 +1,20 @@
 // Copyright (C) 2024 Lavra Holdings Limited - All Rights Reserved
 pragma solidity ^0.8.13;
 
-import {OpportunityProvider} from "../../src/OpportunityProvider.sol";
-import {TokenAmount, OpportunityProviderExecutionWitness} from "../../src/Structs.sol";
+import {OpportunityProvider} from "../opportunity-provider/OpportunityProvider.sol";
+import {TokenAmount, ExecutionWitness} from "../opportunity-provider/Structs.sol";
 import "permit2/interfaces/ISignatureTransfer.sol";
 
 contract OpportunityProviderHarness is OpportunityProvider {
+    constructor(
+        address admin,
+        address expressRelay,
+        address permit2
+    ) OpportunityProvider(admin, expressRelay, permit2) {}
+
     function exposed_transferSellTokens(
         ISignatureTransfer.PermitBatchTransferFrom calldata permit,
-        OpportunityProviderExecutionWitness calldata witness,
+        ExecutionWitness calldata witness,
         bytes calldata signature
     ) public {
         _transferSellTokens(permit, witness, signature);
@@ -16,18 +22,18 @@ contract OpportunityProviderHarness is OpportunityProvider {
 
     function exposed_checkDuplicateTokensTokenAmount(
         TokenAmount[] calldata tokens
-    ) public {
+    ) public pure {
         _checkDuplicateTokens(tokens);
     }
 
     function exposed_checkDuplicateTokensTokenPermissions(
         ISignatureTransfer.TokenPermissions[] calldata tokens
-    ) public {
+    ) public pure {
         _checkDuplicateTokens(tokens);
     }
 
     function exposed_transferBuyTokens(
-        OpportunityProviderExecutionWitness calldata witness
+        ExecutionWitness calldata witness
     ) public {
         _transferBuyTokens(witness);
     }
