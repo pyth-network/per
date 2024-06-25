0e6d11b0921d47c47ea1ec5a7ac7a3cb35d5bf1f
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-06-25 16:47:43 +0200

    Address comments

--- auction-server/migrations/20240624171707_bid_gas_limit.up.sql
+++ auction-server/migrations/20240624171707_bid_gas_limit.up.sql
@@ -1,4 +1,3 @@
--- Add up migration script here
 ALTER TABLE bid ADD COLUMN gas_limit NUMERIC(78, 0);
 UPDATE bid SET gas_limit = 1000000000; -- indefinite gas limit
 ALTER TABLE bid ALTER COLUMN gas_limit SET NOT NULL;
