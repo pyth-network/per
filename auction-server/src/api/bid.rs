a9404e42a9a0c131d15fbd3b85fd05b94b7fe5ba
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-08-16 19:33:27 +0200

    Fix comment for post bid

--- auction-server/src/api/bid.rs
+++ auction-server/src/api/bid.rs
@@ -49,7 +49,7 @@ pub struct BidResult {
 /// Bid on a specific permission key for a specific chain.
 ///
 /// Your bid will be verified by the server. Depending on the outcome of the auction, a transaction
-/// containing will be sent to the blockchain expecting the bid amount to be paid after the call.
+/// containing your bid will be sent to the blockchain expecting the bid amount to be paid in the transaction.
 #[utoipa::path(post, path = "/v1/bids", request_body = Bid, responses(
     (status = 200, description = "Bid was placed successfully", body = BidResult,
     example = json!({"status": "OK", "id": "beedbeed-b346-4fa1-8fab-2541a9e1872d"})),
