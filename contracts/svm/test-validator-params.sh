a1056f468964254d97caa84b575161c4e9f3e5b4
Amin Moghaddam <amin@dourolabs.xyz>
2024-09-17 21:28:33 +0200

    Bump cargo packages (#146)
    * Bump cargo packages
    
    * Small python refactor
    
    * Update dummy program address

--- contracts/svm/test-validator-params.sh
+++ contracts/svm/test-validator-params.sh
@@ -4,7 +4,7 @@ function print_args {
   # express relay program
   echo "--bpf-program PytERJFhAKuNNuaiXkApLfWzwNwSNDACpigT3LwQfou ./target/deploy/express_relay.so"
   # dummy program
-  echo "--bpf-program DuMYMhmEio5twEWgfYFAvJ6pq215LeCc8MMTPPY4DYUR ./target/deploy/dummy.so"
+  echo "--bpf-program DUmmYXYFZugRn2DS4REc5F9UbQNoxYsHP1VMZ6j5U7kZ ./target/deploy/dummy.so"
 
   # Make sure subscribe block is enabled
   echo "--rpc-pubsub-enable-block-subscription"
