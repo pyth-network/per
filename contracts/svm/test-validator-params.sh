3986c122c339fd40e503d3a4e45bd973eb375fea
Amin Moghaddam <amin@dourolabs.xyz>
2024-09-17 18:52:34 +0200

    Update SVM program ids to memorable, stored keys (#145)

--- contracts/svm/test-validator-params.sh
+++ contracts/svm/test-validator-params.sh
@@ -2,9 +2,9 @@
 
 function print_args {
   # express relay program
-  echo "--bpf-program GwEtasTAxdS9neVE4GPUpcwR7DB7AizntQSPcG36ubZM ./target/deploy/express_relay.so"
+  echo "--bpf-program PytERJFhAKuNNuaiXkApLfWzwNwSNDACpigT3LwQfou ./target/deploy/express_relay.so"
   # dummy program
-  echo "--bpf-program HYCgALnu6CM2gkQVopa1HGaNf8Vzbs9bomWRiKP267P3 ./target/deploy/dummy.so"
+  echo "--bpf-program DuMYMhmEio5twEWgfYFAvJ6pq215LeCc8MMTPPY4DYUR ./target/deploy/dummy.so"
 
   # Make sure subscribe block is enabled
   echo "--rpc-pubsub-enable-block-subscription"
