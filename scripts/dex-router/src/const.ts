19543247bc90a85db047bb96a1e4db0e4caa7e61
--systemdf <anirudhtx@gmail.com>
2024-09-30 18:07:30 -0500

    dex router skeleton

---
+++ scripts/dex-router/src/const.ts
@@ -0,0 +1,3 @@
+export const SVM_RPC_ENDPOINTS: Record<string, string> = {
+  solana: "https://api.mainnet-beta.solana.com",
+};
---
+++ scripts/dex-router/src/router/jupiter.ts
@@ -0,0 +1,47 @@
+import { token } from "@coral-xyz/anchor/dist/cjs/utils";
+import { Router, RouterOutput } from "../types";
+import { PublicKey } from "@solana/web3.js";
+
+const jupiterBaseUrl = "https://quote-api.jup.ag/v6/";
+const jupiterQuoteUrl = new URL("quote", jupiterBaseUrl);
+const jupiterSwapIxsUrl = new URL("swap-instructions", jupiterBaseUrl);
+const maxAccounts = 64;
+
+export class JupiterRouter implements Router {
+  async route(
+    chainId: string,
+    tokenIn: PublicKey,
+    tokenOut: PublicKey,
+    amountIn: bigint,
+    executor: PublicKey
+  ): Promise<RouterOutput> {
+    if (chainId !== "solana") {
+      throw new Error("Chain Id not supported");
+    }
+
+    const quoteResponse = await (
+      await fetch(
+        `${jupiterQuoteUrl.toString()}?inputMint=${tokenIn.toBase58()}&outputMint=${tokenOut.toBase58()}&amount=${amountIn}&autoSlippage=true&maxAutoSlippageBps=50&maxAccounts=${maxAccounts}`
+      )
+    ).json();
+
+    const instructions = await (
+      await fetch(jupiterSwapIxsUrl.toString(), {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          quoteResponse,
+          userPublicKey: executor.toBase58(),
+        }),
+      })
+    ).json();
+
+    return {
+      ixs: instructions,
+      amountIn,
+      amountOut: BigInt(quoteResponse.outAmount),
+    };
+  }
+}
