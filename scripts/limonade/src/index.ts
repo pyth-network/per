05d2f20e84f7eedcf0cde6646d82b48e04984d07
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-11-01 19:16:42 +0100

    Fix version

--- scripts/limonade/src/index.ts
+++ scripts/limonade/src/index.ts
@@ -7,7 +7,7 @@ import { limoId, Order } from "@kamino-finance/limo-sdk";
 import bs58 from "bs58";
 import { hideBin } from "yargs/helpers";
 import yargs from "yargs";
-import { Client, OpportunityCreate } from "../../../sdk/js/lib/index";
+import { Client, OpportunityCreate } from "@pythnetwork/express-relay-js";
 import { getPdaAuthority } from "@kamino-finance/limo-sdk/dist/utils";
 
 const lastChange: Record<string, number> = {};
