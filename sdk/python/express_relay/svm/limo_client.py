282f3a986ee35ad8cfba7cefbe33701f5c711abb
Guillermo Bescos <g.bescos@yahoo.com>
2024-11-06 17:25:52 +0000

    fix: output

--- sdk/python/express_relay/svm/limo_client.py
+++ sdk/python/express_relay/svm/limo_client.py
@@ -213,6 +213,7 @@ class LimoClient:
         ixs: List[Instruction] = []
         close_wsol_ixns: List[Instruction] = []
         taker_input_ata: Pubkey
+        output_amount = int(output_amount_decimals * (10**output_mint_decimals))
         if order["state"].input_mint == WRAPPED_SOL_MINT:
             instructions = await self.get_init_if_needed_wsol_create_and_close_ixs(
                 owner=taker, payer=taker, amount_to_deposit_lamports=0
@@ -271,9 +272,7 @@ class LimoClient:
                     input_amount=int(
                         input_amount_decimals * (10**input_mint_decimals)
                     ),
-                    min_output_amount=int(
-                        output_amount_decimals * (10**output_mint_decimals)
-                    ),
+                    min_output_amount=output_amount,
                     tip_amount_permissionless_taking=0
                 ),
                 {
