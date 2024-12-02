2dbc860319db3cd6c80b031acba566d2f7350468
Amin Moghaddam <amin@dourolabs.xyz>
2024-12-02 18:38:47 +0100

    feat: simple searcher for testing the auction server configs (#276)

---
+++ sdk/js/src/examples/testingSearcherPing.ts
@@ -0,0 +1,86 @@
+import * as anchor from "@coral-xyz/anchor";
+import { ComputeBudgetProgram, Keypair } from "@solana/web3.js";
+import {
+  getKeypair,
+  makeParser,
+  SimpleSearcherLimo,
+} from "./simpleSearcherLimo";
+
+class SearcherPinger extends SimpleSearcherLimo {
+  constructor(
+    endpointExpressRelay: string,
+    chainId: string,
+    searcher: Keypair,
+    endpointSvm: string,
+    bid: number,
+    public apiKey?: string
+  ) {
+    super(endpointExpressRelay, chainId, searcher, endpointSvm, bid, apiKey);
+  }
+
+  async opportunityHandler(): Promise<void> {
+    // don't do anything with the opportunity
+  }
+
+  async ping() {
+    if (!this.latestChainUpdate[this.chainId]) {
+      console.log(
+        `No recent blockhash for chain ${this.chainId}, skipping ping`
+      );
+      return;
+    }
+    const feeInstruction = ComputeBudgetProgram.setComputeUnitPrice({
+      microLamports:
+        this.latestChainUpdate[this.chainId].latestPrioritizationFee,
+    });
+    const config = await this.getExpressRelayConfig();
+    const txRaw = new anchor.web3.Transaction().add(feeInstruction);
+    const bid = await this.client.constructSvmBid(
+      txRaw,
+      this.searcher.publicKey,
+      this.searcher.publicKey,
+      this.searcher.publicKey,
+      new anchor.BN(0),
+      new anchor.BN(Math.round(Date.now() / 1000 + 60)),
+      this.chainId,
+      config.relayerSigner,
+      config.feeReceiverRelayer
+    );
+    bid.transaction.recentBlockhash =
+      this.latestChainUpdate[this.chainId].blockhash;
+    bid.transaction.sign(this.searcher);
+    try {
+      const bidId = await this.client.submitBid(bid);
+      console.log(`Successful bid. Bid id ${bidId}`);
+    } catch (error) {
+      console.error(`Failed to ping: ${error}`);
+    }
+  }
+
+  async start() {
+    // run ping every 10 seconds
+    setInterval(async () => {
+      await this.ping();
+    }, 10000);
+
+    await super.start();
+  }
+}
+
+async function run() {
+  const argv = makeParser().parseSync();
+  const searcherKeyPair = getKeypair(argv.privateKey, argv.privateKeyJsonFile);
+  const simpleSearcher = new SearcherPinger(
+    argv.endpointExpressRelay,
+    argv.chainId,
+    searcherKeyPair,
+    argv.endpointSvm,
+    argv.bid,
+    argv.apiKey
+  );
+  await simpleSearcher.start();
+}
+
+if (require.main === module) {
+  run();
+}
