7b760836f3bdf2f7904099d3890877c5756337ae
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-12-05 13:16:55 +0100

    Add more types to api-types

--- auction-server/src/kernel/entities.rs
+++ auction-server/src/kernel/entities.rs
@@ -3,19 +3,6 @@ use {
     base64::Engine,
     bincode::serialized_size,
     ethers::types::Bytes,
-    serde::{
-        Deserialize,
-        Serialize,
-    },
-    serde_with::{
-        base64::{
-            Base64,
-            Standard,
-        },
-        formats::Padded,
-        DeserializeAs,
-        SerializeAs,
-    },
     solana_sdk::{
         packet::PACKET_DATA_SIZE,
         transaction::VersionedTransaction,
@@ -28,16 +15,6 @@ pub type PermissionKey = Bytes;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct PermissionKeySvm(pub [u8; 64]);
-
-impl Serialize for PermissionKeySvm {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        Base64::<Standard, Padded>::serialize_as(&self.0, serializer)
-    }
-}
-
 impl Display for PermissionKeySvm {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
@@ -45,16 +22,6 @@ impl Display for PermissionKeySvm {
             "{}",
             base64::engine::general_purpose::STANDARD.encode(self.0)
         )
-    }
-}
-
-impl<'de> Deserialize<'de> for PermissionKeySvm {
-    fn deserialize<D>(deserializer: D) -> Result<PermissionKeySvm, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let bytes = Base64::<Standard, Padded>::deserialize_as(deserializer)?;
-        Ok(PermissionKeySvm(bytes))
     }
 }
 
