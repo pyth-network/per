18be77516936d900167ca7a6415a2a63ff05c7ca
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-12-06 13:09:19 +0100

    Add simple searcher schema

--- sdk/rust/src/lib.rs
+++ sdk/rust/src/lib.rs
@@ -1,15 +1,24 @@
+use url::Url;
+
 pub struct Client {
-    pub host: String,
+    pub host:    Url,
+    pub api_key: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum ClientError {
+    InvalidHost(String),
 }
 
 impl Client {
-    pub fn new(host: &str) -> Self {
-        Self {
-            host: host.to_string(),
-        }
+    pub fn try_new(host: &str, api_key: Option<&str>) -> Result<Self, ClientError> {
+        Ok(Self {
+            host:    Url::parse(host).map_err(|e| ClientError::InvalidHost(e.to_string()))?,
+            api_key: api_key.map(|s| s.to_string()),
+        })
     }
 
-    pub fn test() {
-        println!("test");
+    pub fn test(&self) {
+        println!("Testing client with host: {:?}", self.host);
     }
 }
