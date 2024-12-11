28fff3b7bf5be5f11a2e591cb799efa6247b8263
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-12-11 18:00:15 +0100

    Fix

--- sdk/rust/src/evm.rs
+++ sdk/rust/src/evm.rs
@@ -37,17 +37,14 @@ use {
 
 abigen!(
     OpportunityAdapter,
-    "./contracts/evm/out/OpportunityAdapter.sol/OpportunityAdapter.json";
+    "./abi/OpportunityAdapter.sol/OpportunityAdapter.json";
     AdapterFactory,
-    "./contracts/evm/out/OpportunityAdapterFactory.sol/OpportunityAdapterFactory.json"
+    "./abi/OpportunityAdapterFactory.sol/OpportunityAdapterFactory.json"
 );
-abigen!(ERC20, "./contracts/evm/out/ERC20.sol/ERC20.json");
-abigen!(WETH9, "./contracts/evm/out/WETH9.sol/WETH9.json");
+abigen!(ERC20, "./abi/ERC20.sol/ERC20.json");
+abigen!(WETH9, "./abi/WETH9.sol/WETH9.json");
 
-abigen!(
-    ExpressRelay,
-    "./contracts/evm/out/ExpressRelay.sol/ExpressRelay.json"
-);
+abigen!(ExpressRelay, "./abi/ExpressRelay.sol/ExpressRelay.json");
 
 #[derive(Debug, Clone)]
 pub struct BidParamsEvm {
