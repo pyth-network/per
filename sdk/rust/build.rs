e2bdcbd60591dbe9f86ff5d8652cd21d0a2d5408
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-12-11 14:48:48 +0100

    Fix build

---
+++ sdk/rust/build.rs
@@ -0,0 +1,30 @@
+use std::process::Command;
+
+
+fn build_evm_contracts() {
+    let contract_setup = r#"
+        cd ../../contracts/evm
+        forge build --via-ir
+    "#;
+    println!("cargo:rerun-if-changed=../../contracts/evm");
+    // Build the contracts and generate the ABIs. This is required for abigen! macro expansions to work.
+    let output = Command::new("sh")
+        .args(["-c", contract_setup])
+        .output()
+        .expect("Failed to run build contracts command");
+    if !output.status.success() {
+        panic!(
+            "Failed to build contracts: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    } else {
+        println!(
+            "Built all solidity contracts  {}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+    }
+}
+
+fn main() {
+    build_evm_contracts();
+}
