2098cd96bf997317aaee82a8e7a9a7e70179f33e
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2024-12-11 18:00:15 +0100

    Fix

--- sdk/rust/build.rs
+++ sdk/rust/build.rs
@@ -1,16 +1,50 @@
-use std::process::Command;
-
+use std::{
+    env,
+    process::Command,
+};
 
 fn build_evm_contracts() {
-    let contract_setup = r#"
-        cd ../../contracts/evm
+    // Get the directory of the current build.rs file
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let mut contract_path = "../../contracts";
+    if current_dir
+        .ancestors()
+        .any(|ancestor| ancestor.ends_with("target"))
+    {
+        // If the build.rs file is in the target directory.
+        contract_path = "../../../contracts";
+    }
+
+    let abis = [
+        "OpportunityAdapter",
+        "OpportunityAdapterFactory",
+        "ERC20",
+        "WETH9",
+        "ExpressRelay",
+    ];
+
+    let contract_setup = format!(
+        r#"
+        cd {}
         forge build --via-ir
-        cp -r ./out ../../sdk/rust/contracts/evm
-    "#;
-    println!("cargo:rerun-if-changed=../../contracts/evm");
+        cd ../sdk/rust
+        mkdir -p abi
+    "#,
+        contract_path
+    );
+
+    // Generate `cp` commands for each ABI
+    let copy_commands: String = abis
+        .iter()
+        .map(|abi| format!("cp -r ../../contracts/evm/out/{}.sol ./abi/.", abi))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    let full_script = format!("{contract_setup}\n{copy_commands}");
+    println!("cargo:rerun-if-changed={}/evm", contract_path);
     // Build the contracts and generate the ABIs. This is required for abigen! macro expansions to work.
     let output = Command::new("sh")
-        .args(["-c", contract_setup])
+        .args(["-c", full_script.as_str()])
         .output()
         .expect("Failed to run build contracts command");
     if !output.status.success() {
