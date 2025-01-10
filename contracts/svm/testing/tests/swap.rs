241c3174e3d8462336e2b206e7ac3bfff157d01d
Guillermo Bescos <g.bescos@yahoo.com>
2025-01-10 21:42:16 +0000

    works

--- contracts/svm/testing/tests/swap.rs
+++ contracts/svm/testing/tests/swap.rs
@@ -5,9 +5,7 @@ use {
             get_associated_token_address_with_program_id,
             spl_associated_token_account::instruction::create_associated_token_account_idempotent,
         },
-        token::{
-            spl_token,
-        },
+        token::spl_token,
         token_2022::spl_token_2022::{
             self,
             instruction::{
