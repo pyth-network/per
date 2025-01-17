95b61baae41c9197d1b1001bbc672bd061fbf9fd
Guillermo Bescos <g.bescos@yahoo.com>
2025-01-17 19:52:39 +0000

    add wallet provider

---
+++ apps/swap/src/components/WalletProvider/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import {
+  ConnectionProvider,
+  WalletProvider as WalletProviderImpl,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  BraveWalletAdapter,
+  BackpackWalletAdapter,
+  CoinbaseWalletAdapter,
+  PhantomWalletAdapter,
+  GlowWalletAdapter,
+  LedgerWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { type ReactNode, useMemo } from "react";
+
+
+type Props = {
+  children?: ReactNode | ReactNode[] | undefined;
+  endpoint: string;
+};
+
+export const WalletProvider = ({
+  endpoint,
+  children,
+}: Props) => {
+
+
+  const wallets = useMemo(
+    () => [
+      new BraveWalletAdapter(),
+      new BackpackWalletAdapter(),
+      new CoinbaseWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter(),
+    ],
+    [],
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProviderImpl wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProviderImpl>
+    </ConnectionProvider>
+  );
+};
