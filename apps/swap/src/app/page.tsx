4f1d64353d70183fc51c9686b7b600c80d2c405c
Guillermo Bescos <g.bescos@yahoo.com>
2025-01-17 22:59:46 +0000

    add shadcn

--- apps/swap/src/app/page.tsx
+++ apps/swap/src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { useWallet } from "@solana/wallet-adapter-react";
-import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import '@solana/wallet-adapter-react-ui/styles.css';
+import { WalletDisconnectButton, WalletMultiButton } from "@/components/WalletButton";
+
 export default function Home() {
   const { publicKey } = useWallet();
   return (
@@ -10,6 +12,7 @@ export default function Home() {
       <WalletMultiButton />
       <WalletDisconnectButton />
       <p>Public Key: {publicKey?.toBase58()}</p>
+      {publicKey && <Button>Click me to sell 1 USD for SOL</Button>}
     </main>
   );
 }
---
+++ apps/swap/src/components/WalletButton/index.tsx
@@ -0,0 +1,11 @@
+import dynamic from 'next/dynamic';
+
+export const WalletDisconnectButton = dynamic(
+    async () => (await import('@solana/wallet-adapter-react-ui')).WalletDisconnectButton,
+    { ssr: false }
+  );
+
+export const WalletMultiButton = dynamic(
+async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+{ ssr: false }
+);
