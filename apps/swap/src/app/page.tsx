5397f3b399a3e83d9d20a2ca3fb8450e2839a226
Guillermo Bescos <g.bescos@yahoo.com>
2025-01-17 23:53:16 +0000

    almost ther

--- apps/swap/src/app/page.tsx
+++ apps/swap/src/app/page.tsx
@@ -1,29 +1,31 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { useWallet } from "@solana/wallet-adapter-react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { WalletDisconnectButton, WalletMultiButton } from "@/components/WalletButton";
 import { Client } from "@pythnetwork/express-relay-js";
-import { PublicKey } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
+import type { SignerWalletAdapterProps } from "@solana/wallet-adapter-base";
 
 const USDC = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
 const USDT = new PublicKey("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB")
 
 export default function Home() {
-  const { publicKey } = useWallet();
+  const { publicKey, signTransaction } = useWallet();
+  const {connection} = useConnection();
   const expressRelayClient = new Client({baseUrl: "https://per-staging.dourolabs.app/"})
   return (
     <main>
       <WalletMultiButton />
       <WalletDisconnectButton />
       <p>Public Key: {publicKey?.toBase58()}</p>
-      {publicKey && <Button onClick={() => handleClick(expressRelayClient, publicKey)}>Click me to sell 1 USDC for USDT</Button>}
+      {publicKey && <Button onClick={() => handleClick(expressRelayClient, publicKey, signTransaction!, connection)}>Click me to sell 1 USDT for USDC</Button>}
     </main>
   );
 }
 
-async function handleClick(expressRelayClient: Client, publicKey: PublicKey) {
+async function handleClick(expressRelayClient: Client, publicKey: PublicKey, signTransaction: SignerWalletAdapterProps['signTransaction'], connection: Connection) {
   console.log("Getting quote...");
   const quote = await expressRelayClient.getQuote({
     chainId: "development-solana",
@@ -36,5 +38,6 @@ async function handleClick(expressRelayClient: Client, publicKey: PublicKey) {
       side: "input",
     },
   });
-  console.log(quote);
+  const signedTransaction = await signTransaction(quote.transaction);
+  connection.sendRawTransaction(signedTransaction.compileMessage().serialize());
 }
