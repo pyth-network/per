53b7b4096bf8d93fdf67821606e1d84ff3dc6f70
Guillermo Bescos <g.bescos@yahoo.com>
2025-01-20 16:49:06 +0000

    refactor callback

--- apps/swap/src/app/page.tsx
+++ apps/swap/src/app/page.tsx
@@ -5,8 +5,8 @@ import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { WalletDisconnectButton, WalletMultiButton } from "@/components/WalletButton";
 import { Client } from "@pythnetwork/express-relay-js";
-import { Connection, PublicKey } from "@solana/web3.js";
-import type { SignerWalletAdapterProps } from "@solana/wallet-adapter-base";
+import { PublicKey } from "@solana/web3.js";
+import { useCallback } from "react";
 
 const USDC = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
 const USDT = new PublicKey("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB")
@@ -15,29 +15,32 @@ export default function Home() {
   const { publicKey, signTransaction } = useWallet();
   const {connection} = useConnection();
   const expressRelayClient = new Client({baseUrl: "https://per-staging.dourolabs.app/"})
+
+  const handleClick = useCallback(async () => {
+    if (!publicKey || !signTransaction) return;
+    console.log("Getting quote...");
+    const quote = await expressRelayClient.getQuote({
+      chainId: "development-solana",
+      inputTokenMint: USDC,
+      outputTokenMint: USDT,
+      router: publicKey,
+      userWallet: publicKey,
+      specifiedTokenAmount: {
+        amount: 1000000,
+        side: "input",
+      },
+    });
+    const signedTransaction = await signTransaction(quote.transaction);
+    connection.sendTransaction(signedTransaction);
+  }, [expressRelayClient, publicKey, signTransaction, connection]);
+
+
   return (
     <main>
       <WalletMultiButton />
       <WalletDisconnectButton />
       <p>Public Key: {publicKey?.toBase58()}</p>
-      {publicKey && <Button onClick={() => handleClick(expressRelayClient, publicKey, signTransaction!, connection)}>Click me to sell 1 USDT for USDC</Button>}
+      {publicKey && <Button onClick={handleClick}>Click me to sell 1 USDT for USDC</Button>}
     </main>
   );
-}
-
-async function handleClick(expressRelayClient: Client, publicKey: PublicKey, signTransaction: SignerWalletAdapterProps['signTransaction'], connection: Connection) {
-  console.log("Getting quote...");
-  const quote = await expressRelayClient.getQuote({
-    chainId: "development-solana",
-    inputTokenMint: USDC,
-    outputTokenMint: USDT,
-    router: publicKey,
-    userWallet: publicKey,
-    specifiedTokenAmount: {
-      amount: 1000000,
-      side: "input",
-    },
-  });
-  const signedTransaction = await signTransaction(quote.transaction);
-  connection.sendTransaction(signedTransaction);
 }
