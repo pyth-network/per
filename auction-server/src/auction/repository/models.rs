a4f5b525da206450fb7506586c42502cb51d0a4a
--systemdf <anirudhtx@gmail.com>
2025-03-21 20:08:02 +0900

    dont need this no more

--- auction-server/src/auction/repository/models.rs
+++ auction-server/src/auction/repository/models.rs
@@ -96,21 +96,6 @@ pub enum BidStatus {
     Cancelled,
 }
 
-impl std::fmt::Display for BidStatus {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            BidStatus::Pending => write!(f, "pending"),
-            BidStatus::AwaitingSignature => write!(f, "awaiting_signature"),
-            BidStatus::Submitted => write!(f, "submitted"),
-            BidStatus::Lost => write!(f, "lost"),
-            BidStatus::Won => write!(f, "won"),
-            BidStatus::Failed => write!(f, "failed"),
-            BidStatus::Expired => write!(f, "expired"),
-            BidStatus::Cancelled => write!(f, "cancelled"),
-        }
-    }
-}
-
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct BundleIndex(pub Option<u32>);
 impl Deref for BundleIndex {
