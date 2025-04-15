8e8c476e4367e221f91240399355ead3c4dba64a
Dewyer <21263440+Dewyer@users.noreply.github.com>
2025-04-15 10:54:49 +0200

    chore: Add RPC test helper (#487)
    chore: use default test chain id
    
    Co-authored-by: Barnabas Ratki <barna@dourolabs.xyz>

--- auction-server/src/kernel/mod.rs
+++ auction-server/src/kernel/mod.rs
@@ -1,3 +1,11 @@
 pub mod db;
 pub mod entities;
+#[cfg(test)]
+pub mod rpc_client_svm_tester;
 pub mod traced_sender_svm;
+
+#[cfg(test)]
+pub mod test_utils {
+    // Default chain id
+    pub const DEFAULT_CHAIN_ID: &str = "solana";
+}
